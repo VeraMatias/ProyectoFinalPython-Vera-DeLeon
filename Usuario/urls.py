c41bf189d7a2b34d1ae1b1f24f9b55b1b5e556ce
MarianoDeLeonPerez <marianogdeleon@gmail.com>
2022-12-16 15:45:38 -0300

    Login ok

--- Usuario/urls.py
+++ Usuario/urls.py
@@ -4,5 +4,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("registro/", register, name="_registro"),
+    path("ingreso/", login_request, name="_ingreso"),
+    path('logout/', LogoutView.as_view(), name='_logout'),
 
 ]
--- Usuario/views.py
+++ Usuario/views.py
@@ -3,20 +3,16 @@ from .forms import *
 from django.http import HttpResponse
 from .models import *
 
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate
+
+
 # Create your views here.
 def register(request):
-<<<<<<< HEAD
-=======
-   
->>>>>>> f3a079c234c639e1c711e4ab70d616b07e2d0ec6
     if request.method=="POST":
         form=RegistroUsuarioForm(request.POST)
         if form.is_valid():
             username=form.cleaned_data.get("username")
-<<<<<<< HEAD
-            print (username)
-=======
->>>>>>> f3a079c234c639e1c711e4ab70d616b07e2d0ec6
             form.save()
             #aca se podria loguear el usuario, con authenticate y login... pero no lo hago
             return render(request, "padre.html", {"mensaje":f"Usuario {username} creado correctamente"})
@@ -27,5 +23,23 @@ def register(request):
         form=RegistroUsuarioForm()
         return render(request, "registrousuario.html", {"form":form})
     
-def pruebar(request):
-    return render(request, "registrousuario.html")
+def login_request(request):
+    if request.method == "POST":
+        form=AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usu=form.cleaned_data.get("username")
+            clave=form.cleaned_data.get("password")
+
+            usuario=authenticate(username=usu, password=clave)#trae un usuario de la base, que tenga ese usuario y ese pass, si existe, lo trae y si no None
+            if usuario is not None:    
+                login(request, usuario)
+                return render(request, 'padre.html', {'mensaje':f"Bienvenido {usuario}" })
+            else:
+                return render(request, 'padre.html', {'mensaje':"Usuario o contraseña incorrectos", 'form':form})
+
+        else:
+            return render(request, 'ingresousuario.html', {'mensaje':"Usuario o contraseña incorrectos", 'form':form})
+
+    else:
+        form = AuthenticationForm()
+    return render(request, "ingresousuario.html", {"form":form})
